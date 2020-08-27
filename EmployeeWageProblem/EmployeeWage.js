1d10a5f3854d108be3025c797dc1961e41067195
karthik-karotra <karthikpatel54@gmail.com>
2020-08-27 08:25:06 +0530

    [Karthik]ADDED : Feature To Calculate Daily Employee Wage

--- EmployeeWageProblem/EmployeeWage.js
+++ EmployeeWageProblem/EmployeeWage.js
@@ -1,7 +1,10 @@
 console.log('Welcome to Employee Wage Simulator');
 //CONSTANTS
 const IS_EMPLOYEE_PRESENT = 1;
+const EMPLOYEE_RATE_PER_HR = 20;
+const EMPLOYEE_HRS = 8;
+var salary;
 let employeeAttendance = Math.floor(Math.random() * 10) % 2;
-console.log(employeeAttendance);
-employeeAttendance === IS_EMPLOYEE_PRESENT ? console.log("Employee Present") : console.log("Employee Absent");
+employeeAttendance === IS_EMPLOYEE_PRESENT ? salary = (EMPLOYEE_HRS * EMPLOYEE_RATE_PER_HR) : salary = 0;
+console.log('Employee Daily Wage is ' + salary + ' Rs');
 //# sourceMappingURL=EmployeeWage.js.map
--- EmployeeWageProblem/EmployeeWage.ts
+++ EmployeeWageProblem/EmployeeWage.ts
@@ -2,7 +2,12 @@ console.log('Welcome to Employee Wage Simulator');
 
 //CONSTANTS
 const IS_EMPLOYEE_PRESENT = 1;
+const EMPLOYEE_RATE_PER_HOUR = 20;
+const EMPLOYEE_HOURS = 8;
 
+//VARIABLES
+var salary;
 let employeeAttendance = Math.floor(Math.random() * 10) % 2;
 
-employeeAttendance === IS_EMPLOYEE_PRESENT ? console.log("Employee Present") : console.log("Employee Absent");
+employeeAttendance === IS_EMPLOYEE_PRESENT ? salary = (EMPLOYEE_HOURS * EMPLOYEE_RATE_PER_HOUR) : salary = 0;
+console.log('Employee Daily Wage is ' + salary + ' Rs');

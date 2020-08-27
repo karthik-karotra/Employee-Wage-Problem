f72c9b4e9f92c5c7c92d17e4e5e339105f4a13bb
karthik-karotra <karthikpatel54@gmail.com>
2020-08-27 10:09:33 +0530

    [Karthik]ADDED : Code To Calculate Wages Of An Employee For A Month

--- EmployeeWageProblem/EmployeeWage.js
+++ EmployeeWageProblem/EmployeeWage.js
@@ -3,22 +3,26 @@ console.log('Welcome to Employee Wage Simulator');
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const EMPLOYEE_RATE_PER_HOUR = 20;
+const NUMBER_OF_WORKING_DAYS = 20;
 //VARIABLES
 var salary;
-var employeeHpurs;
-let employeeCheck = Math.round(Math.random() * 10) % 3;
-console.log(employeeCheck);
-switch (employeeCheck) {
-    case IS_FULL_TIME:
-        employeeHpurs = 8;
-        break;
-    case IS_PART_TIME:
-        employeeHpurs = 4;
-        break;
-    default:
-        employeeHpurs = 0;
-        break;
+var employeeHours;
+let totalSalary = 0;
+for (let day = 1; day <= NUMBER_OF_WORKING_DAYS; day++) {
+    let employeeCheck = Math.round(Math.random() * 10) % 3;
+    switch (employeeCheck) {
+        case IS_FULL_TIME:
+            employeeHours = 8;
+            break;
+        case IS_PART_TIME:
+            employeeHours = 4;
+            break;
+        default:
+            employeeHours = 0;
+            break;
+    }
+    salary = employeeHours * EMPLOYEE_RATE_PER_HOUR;
+    totalSalary += salary;
 }
-salary = employeeHpurs * EMPLOYEE_RATE_PER_HOUR;
-console.log('Employee Daily Wage is ' + salary + ' Rs');
+console.log('Employee wages for a month is ' + totalSalary + ' Rs');
 //# sourceMappingURL=EmployeeWage.js.map

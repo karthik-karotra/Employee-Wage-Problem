ac4df9a63ec698243c7de9430951afd9409f6677
karthik-karotra <karthikpatel54@gmail.com>
2020-08-27 07:55:09 +0530

    [Karthik]ADDED : Functionality To Check If Employee Is Present Or Not

--- EmployeeWageProblem/EmployeeWage.js
+++ EmployeeWageProblem/EmployeeWage.js
@@ -1,2 +1,7 @@
 console.log('Welcome to Employee Wage Simulator');
+//CONSTANTS
+const IS_EMPLOYEE_PRESENT = 1;
+let employeeAttendance = Math.floor(Math.random() * 10) % 2;
+console.log(employeeAttendance);
+employeeAttendance === IS_EMPLOYEE_PRESENT ? console.log("Employee Present") : console.log("Employee Absent");
 //# sourceMappingURL=EmployeeWage.js.map
--- EmployeeWageProblem/EmployeeWage.ts
+++ EmployeeWageProblem/EmployeeWage.ts
@@ -1,1 +1,8 @@
 console.log('Welcome to Employee Wage Simulator');
+
+//CONSTANTS
+const IS_EMPLOYEE_PRESENT = 1;
+
+let employeeAttendance = Math.floor(Math.random() * 10) % 2;
+
+employeeAttendance === IS_EMPLOYEE_PRESENT ? console.log("Employee Present") : console.log("Employee Absent");
